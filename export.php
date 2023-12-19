d536c925baff6f44aff9f7799dd07fa2766a5b9e
yoru <fikri36987@gmail.com>
2023-12-19 15:14:03 +0700

    add: first commit

---
+++ export.php
@@ -0,0 +1,78 @@
+<?php 
+    require "core/function.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <h2>Inventory Barang</h2>
+        <div class="data-table datatable-dark">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="1">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Nama Barang</th>
+                                                <th>Jenis</th>
+                                                <th>Stock</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <!-- Tampilkan data dari database ke halaman web -->
+                                            <?php 
+                                                $selectALL = mysqli_query($conn, "SELECT * FROM barang");
+                                                while ($data = mysqli_fetch_array($selectALL)) {
+                                                    $idb = $data['idbarang'];
+                                                    $namabarang = $data['namabarang'];
+                                                    $jenis = $data['jenisbarang'];
+                                                    $stock = $data['stock'];
+                                            ?>
+                                            
+                                                    <tr>
+                                                        <td><?=$idb;?></td>
+                                                        <td><?=$namabarang;?></td>
+                                                        <td><?=$jenis;?></td>
+                                                        <td><?=$stock;?></td>
+                                                    </tr>
+                                            <?php 
+                                            } // Tutup kurung kurawal untuk while loop
+                                            ?>
+                                        </tbody>
+                                    </table>
+        </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+    <script>
+        $(document).ready(function() {
+        $('#dataTable').DataTable( {
+            dom: 'Bfrtip',
+            buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
+    </script>
+
+
+
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,92 @@
+<?php 
+    require "core/function.php";
+    
+
+    //ambil data
+    if (isset($_POST['register'])) {
+        $email = $_POST['email'];
+        $pass = $_POST['password'];
+
+        if (!empty(trim($email)) && !empty(trim($pass))) {
+            $insert = mysqli_query($conn, "INSERT INTO  login (email,password) values ('$email','$pass')");
+            if ($insert) {
+                header('location:login.php');
+            }
+            
+        }else {
+            echo'
+                <script>
+                    alert("gagal")
+                    windows.location.href="register.php"
+                </script>
+            ';
+            
+        }
+        
+         
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Register</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-7">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Create Account</h3></div>
+                                    <div class="card-body">
+                                        <form method="post">
+                                            <div class="form-group">
+                                                <label class="small mb-1" for="inputEmailAddress">Email</label>
+                                                <input class="form-control py-4" id="inputEmailAddress" type="email" name="email" aria-describedby="emailHelp" placeholder="Enter email address" required/>
+                                            </div>
+                                            <div class="form-row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label class="small mb-1" for="inputPassword">Password</label>
+                                                        <input class="form-control py-4" id="inputPassword" type="password" name="password" placeholder="Enter password" required/>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label class="small mb-1" for="inputConfirmPassword">Confirm Password</label>
+                                                        <input class="form-control py-4" id="inputConfirmPassword" type="password" placeholder="Confirm password" required/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group mt-4 mb-0">
+                                                <button class="btn btn-primary btn-block" href="login.html" name="register">Create Account</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center">
+                                        <div class="small"><a href="login.php">Have an account? Go to login</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
